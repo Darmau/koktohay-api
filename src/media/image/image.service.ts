7256d9fd8f70a2abc20ad3bfaba30456e262bea8
李大毛 <dev@darmau.design>
2023-10-28 19:31:30 +0800

    dar-37: 修复图片相关接口中仍用到环境变量的地方

--- src/media/image/image.service.ts
+++ src/media/image/image.service.ts
@@ -4,36 +4,24 @@ import extractKey from '@/utils/extract-key';
 import getLocation from '@/utils/get-location';
 import UploadRawImage from '@/utils/upload-raw-image';
 import uploadToR2 from '@/utils/upload-to-R2';
-import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
+import {HttpException, HttpStatus, Injectable, Logger} from '@nestjs/common';
+import {InjectModel} from '@nestjs/mongoose';
 import exifr from 'exifr';
-import { Model } from 'mongoose';
+import {Model} from 'mongoose';
 import sharp from 'sharp';
-import { Image } from '@/schemas/image.schema';
-import { ConfigService } from '@/settings/config/config.service';
+import {Image} from '@/schemas/image.schema';
+import {ConfigService} from '@/settings/config/config.service';
 
 @Injectable()
 export class ImageService {
   private readonly logger = new Logger(ImageService.name);
   private EXIF = null;
   private location = null;
-  private prefix = process.env.S3_URL_PREFIX;
 
   constructor(
-    @InjectModel('Image') private imageModel: Model<Image>,
-    private readonly configService: ConfigService,
-  ) {}
-
-  private addPrefixUrl(formats) {
-    return Object.entries(formats).reduce((acc, [key, value]) => {
-      acc[key] = `${this.prefix}/${value}`;
-      return acc;
-    }, {});
-  }
-
-  // 获取地图API
-  private async getMapAPI() {
-    return await this.configService.getMapApi();
+      @InjectModel('Image') private imageModel: Model<Image>,
+      private readonly configService: ConfigService,
+  ) {
   }
 
   // 上传原始图片并提取信息
@@ -41,8 +29,8 @@ export class ImageService {
     // 检查mine type，如果是heif格式返回报错
     if (file.mimetype === 'image/heif' || file.mimetype === 'image/heic') {
       throw new HttpException(
-        'HEIF format is not supported',
-        HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+          'HEIF format is not supported',
+          HttpStatus.UNSUPPORTED_MEDIA_TYPE,
       );
     }
 
@@ -50,7 +38,10 @@ export class ImageService {
     const S3Config = await this.configService.getS3Config();
 
     // 上传原始文件
-    const { upload_time, key } = await new UploadRawImage(file, S3Config).upload();
+    const {
+      upload_time,
+      key
+    } = await new UploadRawImage(file, S3Config).upload();
 
     // 提取基础信息
     const metadata = await sharp(file.buffer).metadata();
@@ -59,6 +50,7 @@ export class ImageService {
       const svgImageData = new this.imageModel({
         raw: key,
         format: metadata.format,
+        alt: file.originalname,
         upload_time: upload_time,
         size: file.size,
         width: metadata.width,
@@ -79,9 +71,9 @@ export class ImageService {
       const AMAP_KEY = await this.getMapAPI();
       // 获取地理位置信息
       this.location = await getLocation(
-        AMAP_KEY,
-        this.EXIF.longitude,
-        this.EXIF.latitude,
+          AMAP_KEY,
+          this.EXIF.longitude,
+          this.EXIF.latitude,
       );
     }
 
@@ -95,6 +87,7 @@ export class ImageService {
         width: metadata.width,
         height: metadata.height,
         hasAlpha: metadata.hasAlpha,
+        alt: file.originalname,
         exif: {
           maker: this.EXIF?.Make,
           model: this.EXIF?.Model,
@@ -116,19 +109,27 @@ export class ImageService {
     } catch (error) {
       this.logger.error(`Failed to save RAW image to MongoDB: ${error}`);
       throw new HttpException(
-        'Failed to save RAW image to MongoDB',
-        HttpStatus.INTERNAL_SERVER_ERROR,
+          'Failed to save RAW image to MongoDB',
+          HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
 
   // 获取指定图片的信息
-  async getImage(id: string): Promise<Image> {
+  async getImage(id: string) {
     const image = await this.imageModel.findById(id).exec();
     if (!image) {
       throw new HttpException('Image not found', HttpStatus.NOT_FOUND);
     }
-    return image;
+    const S3Config = await this.configService.getS3Config();
+    const imageObject = image.toObject();
+    return {
+      ...imageObject,
+      raw: await this.addPrefixUrl(imageObject.raw, S3Config.S3_URL_PREFIX),
+      large: await this.addPrefixUrl(imageObject.large, S3Config.S3_URL_PREFIX),
+      medium: await this.addPrefixUrl(imageObject.medium, S3Config.S3_URL_PREFIX),
+      small: await this.addPrefixUrl(imageObject.small, S3Config.S3_URL_PREFIX),
+    };
   }
 
   // 删除指定图片
@@ -145,8 +146,8 @@ export class ImageService {
     } catch (error) {
       this.logger.error(`Failed to delete images from R2: ${error}`);
       throw new HttpException(
-        'Failed to delete images from R2',
-        HttpStatus.SERVICE_UNAVAILABLE,
+          'Failed to delete images from R2',
+          HttpStatus.SERVICE_UNAVAILABLE,
       );
     }
   }
@@ -154,22 +155,18 @@ export class ImageService {
   // 获取图片thumbnail列表
   async getLatestThumbnails(limit: number = 10, page: number = 1) {
     const images = await this.imageModel
-      .find()
-      .sort({ upload_time: -1 })
-      .skip((page - 1) * limit)
-      .limit(limit)
-      .exec();
+    .find()
+    .sort({upload_time: -1})
+    .skip((page - 1) * limit)
+    .limit(limit)
+    .exec();
     if (!images) {
       throw new HttpException('No image for now', HttpStatus.NOT_FOUND);
     }
-    const thumbnails = images.map((image) => {
-      const urls = {};
-      for (const format in image.small) {
-        urls[format] = `${this.prefix}/${image.small[format]}`;
-      }
-      return urls;
-    });
-    return thumbnails;
+    const S3Config = await this.configService.getS3Config();
+    return await Promise.all(images.map(async (image) => {
+      return await this.addPrefixUrl(image.small, S3Config.S3_URL_PREFIX);
+    }));
   }
 
   // 替换图片的raw字段
@@ -179,7 +176,7 @@ export class ImageService {
     if (!imageData) {
       throw new HttpException('Image not found', HttpStatus.NOT_FOUND);
     }
-    const { folder, fileName } = extractFolderName(imageData.raw);
+    const {folder, fileName} = extractFolderName(imageData.raw);
     const key = `${folder}/${fileName}-raw.${format}`;
     // 获取cloudflare配置
     const S3Config = await this.configService.getS3Config();
@@ -206,11 +203,30 @@ export class ImageService {
       this.logger.error(`Image not found: ${id}`);
       throw new HttpException('Image not found', HttpStatus.NOT_FOUND);
     }
+    const S3Config = await this.configService.getS3Config();
     return {
-      raw: `${this.prefix}/${image.raw}`,
-      large: this.addPrefixUrl(image.large),
-      medium: this.addPrefixUrl(image.medium),
-      small: this.addPrefixUrl(image.small),
+      raw: await this.addPrefixUrl(image.raw, S3Config.S3_URL_PREFIX),
+      large: await this.addPrefixUrl(image.large, S3Config.S3_URL_PREFIX),
+      medium: await this.addPrefixUrl(image.medium, S3Config.S3_URL_PREFIX),
+      small: await this.addPrefixUrl(image.small, S3Config.S3_URL_PREFIX),
     };
+  }
+
+  private async addPrefixUrl(formats, prefix: string) {
+    // raw
+    if (typeof formats === 'string') {
+      return `${prefix}/${formats}`;
+    } else {
+      // 其他尺寸
+      const urls =  Object.entries(formats).reduce((acc, [key, value]) => {
+        return { ...acc, [key]: `${prefix}/${value}` };
+      }, {});
+      return urls;
+    }
+  }
+
+  // 获取地图API
+  private async getMapAPI() {
+    return await this.configService.getMapApi();
   }
 }
