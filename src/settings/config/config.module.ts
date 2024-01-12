9f314baaf2d93bced4b11254fe75a3b7426f48b2
李大毛 <dev@darmau.design>
2024-01-12 23:58:49 +0800

    refactor: 迁移config至postgres

--- src/settings/config/config.module.ts
+++ src/settings/config/config.module.ts
@@ -3,10 +3,12 @@ import { ConfigController } from './config.controller';
 import { ConfigService } from './config.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SettingsSchema } from '@/schemas/settings.schema';
+import {PrismaModule} from "@/prisma/prisma.module";
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Settings', schema: SettingsSchema }]),
+      PrismaModule
   ],
   controllers: [ConfigController],
   providers: [ConfigService],
