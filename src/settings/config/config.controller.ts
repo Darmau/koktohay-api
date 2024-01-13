43f307aa1b0a07663e52a1025f8bcc6ef2537de3
李大毛 <dev@darmau.design>
2024-01-13 11:40:58 +0800

    perf: 配置支持缓存提升性能 (#13)
    * perf: setup cache manager
    
    * perf: 配置支持缓存
    
    * perf: language config cache

--- src/settings/config/config.controller.ts
+++ src/settings/config/config.controller.ts
@@ -2,52 +2,78 @@ import {
   Body,
   Controller,
   Delete,
-  Get,
+  Get, Inject,
   Patch,
   Post,
   Query
 } from '@nestjs/common';
-import { ConfigService } from './config.service';
-import { AddS3ConfigDto } from '@/dto/addS3Config.dto';
+import {ConfigService} from './config.service';
 import {SendConfigDto} from "@/dto/sendConfig.dto";
+import {CACHE_MANAGER} from "@nestjs/cache-manager";
+import {Cache} from "cache-manager";
 
 @Controller('config')
 export class ConfigController {
-  constructor(private readonly configService: ConfigService) {}
+
+  constructor(
+      @Inject(CACHE_MANAGER) private cacheManager: Cache,
+      private readonly configService: ConfigService,
+  ) {
+  }
 
   // 添加配置
   @Post()
   async addKey(@Body() sendConfigDto: SendConfigDto) {
-    return await this.configService.addKey(
-      sendConfigDto.name,
-      sendConfigDto.value,
+    const newConfig = await this.configService.addKey(
+        sendConfigDto.name,
+        sendConfigDto.value,
     );
+    newConfig && await this.cacheManager.set(
+          sendConfigDto.name,
+          {name: sendConfigDto.name, value: sendConfigDto.value},
+          0);
+    return newConfig;
   }
 
   // 删除配置
   @Delete()
   async deleteKey(@Query('name') name: string) {
-    return await this.configService.deleteKey(name);
+    const config = await this.configService.deleteKey(name);
+    await this.cacheManager.del(name);
+    return config;
   }
 
   // 获取配置
   @Get()
   async getKeyValue(@Query('name') name: string) {
-    return await this.configService.getKeyValue(name);
+    const configFromCache = await this.cacheManager.get(name);
+    if (configFromCache) {
+      return configFromCache;
+    }
+    const config = await this.configService.getKeyValue(name);
+    config && await this.cacheManager.set(name, config, 0);
+    return config;
   }
 
-   // 更新配置
+  // 更新配置
   @Patch()
   async updateKey(@Body() sendConfigDto: SendConfigDto) {
-    return await this.configService.updateKey(
-      sendConfigDto.name,
-      sendConfigDto.value,
+    const updateConfig = await this.configService.updateKey(
+        sendConfigDto.name,
+        sendConfigDto.value,
     );
+    updateConfig && await this.cacheManager.set(
+          sendConfigDto.name,
+          {name: sendConfigDto.name, value: sendConfigDto.value},
+          0);
+    return updateConfig;
   }
 
   // 获取所有S3相关配置
   @Get('storage')
   async getS3Config() {
-    return await this.configService.getS3Config();
+    const S3Config = await this.configService.getS3Config();
+    S3Config && await this.cacheManager.set('S3Config', S3Config, 0);
+    return S3Config;
   }
 }
