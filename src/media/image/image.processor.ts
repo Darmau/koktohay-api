a1347c6042fa7ad53724ea0a263e0268d4c18113
李大毛 <github@darmau.co>
2024-01-25 14:00:55 +0800

    perf: 增加thumbnail规格，并只转换为webp

--- src/media/image/image.processor.ts
+++ src/media/image/image.processor.ts
@@ -11,14 +11,9 @@ import {PrismaService} from "@/prisma/prisma.service";
 // 本文件用于异步执行图片处理任务
 @Processor('image')
 export class ImageProcessor {
-  constructor(
-      private readonly configService: ConfigService,
-      private prisma: PrismaService
-  ) {}
-
   private readonly logger = new Logger(ImageProcessor.name);
-
   private readonly resolutions: Map<string, number> = new Map([
+    ['thumbnail', 160],
     ['small', 640],
     ['medium', 1280],
     ['large', 2560],
@@ -27,6 +22,12 @@ export class ImageProcessor {
     opaque: ['jpeg', 'webp', 'avif'],
     transparent: ['png', 'webp', 'avif'],
   };
+
+  constructor(
+      private readonly configService: ConfigService,
+      private prisma: PrismaService
+  ) {
+  }
 
   // 本队列任务接收文件，数据库id，文件夹名，文件名
   @Process('image-process')
@@ -37,7 +38,7 @@ export class ImageProcessor {
         id: job.data.id
       }
     });
-    const { folder, fileName } = extractFolderName(imageData);
+    const {folder, fileName} = extractFolderName(imageData);
 
     // 如果是svg和gif格式，停止处理
     if (imageData.format === 'svg' || imageData.format === 'gif') {
@@ -65,7 +66,7 @@ export class ImageProcessor {
     const imageFile = await stream.Body.transformToByteArray();
 
     const formatList =
-      this.formats[imageData.has_alpha ? 'transparent' : 'opaque'];
+        this.formats[imageData.has_alpha ? 'transparent' : 'opaque'];
 
     for (const [label, target] of this.resolutions) {
       const edge = imageData.width > imageData.height ? 'width' : 'height';
@@ -73,6 +74,15 @@ export class ImageProcessor {
       const resolution = imageData[edge] > target ? target : imageData[edge];
 
       const resizedImage = await this.resizeImage(resolution, edge, imageFile);
+
+      // 对于label是thumbnail的图片，只上传webp格式，跳过下方的for循环，继续执行下一个label
+      if (label === 'thumbnail') {
+        const convertedImage = await this.convertImage('webp', resizedImage);
+        const key = `${folder}/${fileName}-${label}.webp`;
+        this.logger.debug(`Uploading thumbnail ${key}`);
+        await uploadToR2(key, convertedImage, S3Config);
+        continue;
+      }
 
       for (const format of formatList) {
         const convertedImage = await this.convertImage(format, resizedImage);
@@ -88,14 +98,14 @@ export class ImageProcessor {
 
   // 缩放图片
   private async resizeImage(
-    resolution: number,
-    edge: string,
-    image: Uint8Array,
+      resolution: number,
+      edge: string,
+      image: Uint8Array,
   ): Promise<Buffer> {
     try {
       const resizedImage = await sharp(image)
-        .resize({ [edge]: resolution })
-        .toBuffer();
+      .resize({[edge]: resolution})
+      .toBuffer();
       if (!resizedImage || resizedImage.length === 0) {
         throw new Error('Resized image is missing or empty');
       }
@@ -111,22 +121,22 @@ export class ImageProcessor {
     switch (format) {
       case 'jpeg': {
         return await sharp(image)
-          .jpeg({ mozjpeg: true, progressive: true, quality: 75 })
-          .toBuffer();
+        .jpeg({mozjpeg: true, progressive: true, quality: 75})
+        .toBuffer();
       }
       case 'png': {
-        return await sharp(image).png({ progressive: true }).toBuffer();
+        return await sharp(image).png({progressive: true}).toBuffer();
       }
       case 'webp': {
-        return await sharp(image).webp({ quality: 80 }).toBuffer();
+        return await sharp(image).webp({quality: 80}).toBuffer();
       }
       case 'avif': {
-        return await sharp(image).avif({ quality: 60 }).toBuffer();
+        return await sharp(image).avif({quality: 60}).toBuffer();
       }
       default: {
         return await sharp(image)
-          .jpeg({ mozjpeg: true, progressive: true })
-          .toBuffer();
+        .jpeg({mozjpeg: true, progressive: true})
+        .toBuffer();
       }
     }
   }
