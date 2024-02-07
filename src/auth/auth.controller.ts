7b75b19b63f52dde37e91d56fff1d1e34d8583fe
李大毛 <github@darmau.co>
2024-02-07 22:16:06 +0800

    Dar 106 auth module (#25)
    * delete unused interface
    
    * import Prisma module
    
    * delete unused type
    
    * refactor
    
    * add supabase
    
    * feat: setup email signup and login
    
    * feat: add third party login
    
    * feat: add third party login
    
    * 修改路由
    
    * 修改路由
    
    * update user role
    
    * update user role
    
    * feat: add auth guard
    
    * set user_id unique
    
    * enable user_id index
    
    * 将返回的user id传入request
    
    * set user_id unique
    
    * feat: reader guard
    
    * feat: admin guard
    
    * feat: banned guard
    
    * remove unused logger
    
    * refactor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Inject, Param, Post} from '@nestjs/common';
+import {CACHE_MANAGER} from "@nestjs/cache-manager";
+import {Cache} from "cache-manager";
+import {AuthService} from "@/auth/auth.service";
+import {SignupDto} from "@/auth/signup.dto";
+import {Provider} from "@supabase/supabase-js";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+      @Inject(CACHE_MANAGER) private cacheManager: Cache,
+      private readonly authService: AuthService
+  ) {}
+
+  // 注册 /auth/signup POST
+  @Post('signup')
+  async register(@Body() registerDto: SignupDto) {
+    return await this.authService.signup(
+        registerDto.email,
+        registerDto.password
+    );
+  }
+
+  // 登录 /auth/login POST
+  @Post('login')
+  async login(@Body() registerDto: SignupDto) {
+    return await this.authService.login(
+        registerDto.email,
+        registerDto.password
+    );
+  }
+
+  // 第三方登录 /auth/oauth/github POST
+  @Post('oauth/:provider')
+  async oauthLogin(@Param('provider') provider: Provider){
+    return await this.authService.oauthLogin(
+        provider
+    );
+  }
+}
--- src/enum/image-size.type.ts
+++
@@ -1,1 +0,0 @@
-export type ImageSize = 'raw' | 'small' | 'medium' | 'large';
