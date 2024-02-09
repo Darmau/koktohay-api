28a0bd5ad259f9a803e4c12015c4b510ef09f7ed
李大毛 <github@darmau.co>
2024-02-09 13:54:38 +0800

    fix: remove delay

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,9 +4,7 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit{
   async onModuleInit() {
-    // 增加3秒延迟，等待数据库连接
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    console.log('PrismaService is connected');
     await this.$connect();
   }
+
 }
