9a52337c18cebcf9520885e5f80715ad322513a5
李大毛 <github@darmau.co>
2024-05-11 11:12:14 +0900

    fix: positive int type

--- src/pipe/positiveInt.pipe.ts
+++ src/pipe/positiveInt.pipe.ts
@@ -2,7 +2,7 @@ import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class PositiveIntPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata): any {
+  transform(value: number, metadata: ArgumentMetadata): number {
     if (metadata.type === 'query') {
       if (metadata.data === 'limit' && value < 1) {
         return 10;
